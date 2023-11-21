a81317cab9a512eaa2ca7962029d941c742aa83d
Kolin136 <jsowb136@gmail.com>
2023-11-21 12:55:12 +0900

    프로젝트 생성

---
+++ src/main/java/com/team/gameblog/GameBlogApplication.java
@@ -0,0 +1,13 @@
+package com.team.gameblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team/gameblog/GameBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.gameblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

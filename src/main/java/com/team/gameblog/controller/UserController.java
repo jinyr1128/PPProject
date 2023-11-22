35f43a51bb00f81d0092b1d0cf5ef2c60ef099c5
Kolin136 <jsowb136@gmail.com>
2023-11-22 21:01:28 +0900

    커스텀 예외 추가

--- src/main/java/com/team/gameblog/controller/UserController.java
+++ src/main/java/com/team/gameblog/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.team.gameblog.controller;
 
+import com.team.gameblog.dto.user.ProfileRequestDto;
 import com.team.gameblog.dto.user.SignupRequestDto;
 import com.team.gameblog.entity.User;
+import com.team.gameblog.security.UserDetailsImpl;
 import com.team.gameblog.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -21,10 +24,11 @@ public class UserController {
     private final UserService userService;
 
     //회원가입
+    @ResponseBody
     @PostMapping("/user/signup")
-    public ResponseEntity<?> signup(@RequestBody @Valid SignupRequestDto requestDto, BindingResult bindingResult){
+    public ResponseEntity<?> signup(@RequestBody @Valid SignupRequestDto requestDto, BindingResult bindingResult) {
 
-        if (bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(bindingResult.getFieldErrors());
         }
 
@@ -48,14 +52,26 @@ public class UserController {
 
     // 로그아웃후 전체 게시글 페이지
     @GetMapping("/logout")
-    public String logout(){
+    public String logout() {
 
         return "";
     }
 
-    //프로필 생성
-//    @PostMapping("/profile")
-//    public PorofileResponseDto createPrfile(){
-//        return userService.createProfile();
-//    }
+    @ResponseBody
+    @PutMapping("/user/profile/{id}")
+    public ResponseEntity<?> updateProfile(@PathVariable Long id,
+                                              @RequestBody @Valid ProfileRequestDto requestDto,
+                                              @AuthenticationPrincipal UserDetailsImpl userDetails,
+                                              BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(bindingResult.getFieldErrors());
+        }
+
+        userService.updateProfile(id,requestDto,userDetails.getUser());
+        return ResponseEntity.ok("프로필 수정 완료");
+
+    }
+
 }
+
--- src/main/java/com/team/gameblog/dto/user/ProfileRequestDto.java
+++ src/main/java/com/team/gameblog/dto/user/ProfileRequestDto.java
@@ -4,22 +4,10 @@ import jakarta.validation.constraints.*;
 import lombok.Getter;
 
 @Getter
-public class SignupRequestDto {
+public class ProfileRequestDto {
 
-
-    @NotBlank
     @Email(message = "이메일 형식에 맞게 적어주세요")
     private String email;
-
-
-    @Pattern(regexp ="[a-zA-Z0-9]*$",message ="비밀번호 허용 문자에 맞게 해주세요")
-    @Size(min=8, max=15,message ="비밀번호 8자 이상 15자 이하이어야 합니다")
-    @NotBlank
-    private String password;
-
-    @NotBlank
-    private String passwordCheck;
-
 
     @Pattern(regexp ="^[a-z]+[0-9]*$",message ="아이디 허용문자에 맞게 적어주세요")
     @Size(min=4, max=10,message ="아이디 4자 이상 10자 이하이어야 합니다")
@@ -28,5 +16,6 @@ public class SignupRequestDto {
 
     @Max(value = 300,message = "최대 300자 까지 입니다.")
     private String profile;
+
 
 }

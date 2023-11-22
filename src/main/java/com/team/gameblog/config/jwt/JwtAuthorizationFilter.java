ee5b3b5e717da65658caa75d1ff052fc3bbfaa15
Kolin136 <jsowb136@gmail.com>
2023-11-22 16:00:01 +0900

    FEAT: 로그인 관련 시큐리티 추가

--- src/main/java/com/team/gameblog/config/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/team/gameblog/config/jwt/JwtAuthorizationFilter.java
@@ -1,4 +1,87 @@
 package com.team.gameblog.config.jwt;
 
-public class JwtAuthorizationFilter {
+
+import com.team.gameblog.security.UserDetailsServiceImpl;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Slf4j(topic = "JWT 검증 및 인가")
+public class JwtAuthorizationFilter extends OncePerRequestFilter {  //OncePerRequestFilter 요청당  해당 필터가 한번만 수행
+
+    private final JwtUtil jwtUtil;
+
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public JwtAuthorizationFilter(JwtUtil jwtUtil, UserDetailsServiceImpl userDetailsService) {
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain) throws ServletException, IOException {
+
+        String tokenValue = jwtUtil.getJWtHeader(req);
+
+        if (StringUtils.hasText(tokenValue)){
+
+            try {
+                jwtUtil.validateToken(tokenValue);
+            }catch (JwtException e){
+                log.error(e.getMessage());
+                res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                res.setContentType("application/json");
+                res.setCharacterEncoding("utf-8");
+                PrintWriter writer = res.getWriter();
+                writer.println(e.getMessage());
+                return;
+            }
+
+
+            Claims info = jwtUtil.getUserInfoFromToken(tokenValue);
+
+            //info.getSubject()는 인증 단계에서 토큰 생성할때 넣은 username 가져온다.
+            try {
+                setAuthentication(info.getSubject());
+                log.info("인가 시작");
+            } catch (Exception e) {
+                log.error(e.getMessage());
+                return;
+            }
+        }
+
+        filterChain.doFilter(req, res);
+    }
+
+
+    public void setAuthentication(String username) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        Authentication authentication = createAuthentication(username);
+        context.setAuthentication(authentication);
+        SecurityContextHolder.setContext(context);
+        log.info("인가 성공");
+    }
+
+
+    private Authentication createAuthentication(String username) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
+
 }

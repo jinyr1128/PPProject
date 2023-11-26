802aaa95eb133fc74adb9e0c8129cf0b37d4673f
jinyr1128 <jinyr1128@gmail.com>
2023-11-26 16:51:44 +0900

    RequestDTO구현

--- src/main/java/com/team/gameblog/dto/comment/CommentRequestDto.java
+++ src/main/java/com/team/gameblog/dto/comment/CommentRequestDto.java
@@ -1,4 +1,12 @@
 package com.team.gameblog.dto.comment;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CommentRequestDto {
+    private String comment; // 댓글 내용
+    private Long articleId; // 게시글 ID (댓글을 달 게시글)
+    private Long userId;// 사용자 ID (댓글 작성자)
 }
